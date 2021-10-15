053656186a4d1bed1a7cbd1db04e549dffdfb124
prrao87 <prrao87@gmail.com>
2021-10-15 18:52:06 -0400

    Update phone calls example with Neo4j Python driver

--- phone_calls/neo4j_graph.py
+++ phone_calls/neo4j_graph.py
@@ -1,119 +1,124 @@
+"""
+Use the Neo4j official Python bolt driver to populate a Neo4j graph
+"""
 import json
-from py2neo import Graph
-
-uri = "bolt://localhost:7687"
-
-
-def build_graph():
-    # Start a Neo4j graph session and pass external data to build graph
-    graph = Graph(uri, user="neo4j", password="local")
-    # Set indexes and constraints to improve query performance
-    set_indexes(graph)
-    set_constraints(graph)
-    # Build graph
-    tx = graph.begin()
-    for case in INPUTS:
-        data = parse_input_json(case['file'])
-        query = case['template'](data)
-        if query:
-            tx.run(query, {"data": data})
-            tx.commit()
-            print(f"Generated nodes/relationships using data from {case['file']}")
-        tx = graph.begin()
+from neo4j import GraphDatabase
+from neo4j.work.transaction import Transaction
+from typing import Dict
 
 
-def set_indexes(graph):
-    # Create indexes on specific node properties to improve performance
-    indexes = [
-        "CREATE INDEX ON :Company(name)",
-    ]
-    for index in indexes:
-        graph.evaluate(index)
+class Neo4jConnection:
+    def __init__(
+        self, uri: str, user: str, password: str, filenames: Dict[str, str]
+    ) -> None:
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+        self.filenames = filenames
 
+    def close(self) -> None:
+        self.driver.close()
 
-def set_constraints(graph):
-    # Set uniqueness constraints on key properties
-    constraints = [
-        "CREATE CONSTRAINT ON (p:Person) ASSERT p.personID IS UNIQUE",
-    ]
-    for constraint in constraints:
-        graph.evaluate(constraint)
+    def run(self) -> None:
+        with self.driver.session() as session:
+            session.write_transaction(self._create_indexes_and_constraints)
+            session.write_transaction(
+                self._create_companies, self.filenames["companies"]
+            )
+            session.write_transaction(self._create_people, self.filenames["people"])
+            session.write_transaction(
+                self._create_contracts, self.filenames["contracts"]
+            )
+            session.write_transaction(self._create_calls, self.filenames["calls"])
 
+    @staticmethod
+    def _create_indexes_and_constraints(tx: Transaction) -> None:
+        "Set indexes to improve performance of adding nodes as the graph gets larger"
+        index_queries = [
+            # indexes
+            "CREATE INDEX company_name IF NOT EXISTS FOR (c:Company) ON (c.name) ",
+            # constraints
+            "CREATE CONSTRAINT IF NOT EXISTS ON (p:Person) ASSERT p.personID IS UNIQUE ",
+        ]
+        for query in index_queries:
+            tx.run(query)
 
-def company_template(data):
-    # Query to create company nodes
-    query = """
-        UNWIND $data AS d
-        MERGE (c:Company {name: d.name})
-    """
-    return query
+    @staticmethod
+    def _create_companies(tx: Transaction, filename: str) -> None:
+        data = parse_input_json(filename)
+        tx.run(
+            """
+            UNWIND $data AS d
+            MERGE (c:Company {name: d.name})
+            """,
+            data=data,
+        )
 
+    @staticmethod
+    def _create_people(tx: Transaction, filename: str) -> None:
+        data = parse_input_json(filename)
+        tx.run(
+            """
+            UNWIND $data as d
+            MERGE (p:Person {personID: d.phone_number})
+              SET p.firstName = d.first_name, p.lastName = d.last_name, p.city = d.city, p.age = d.age
+              SET p.fullName = d.first_name + ' ' + d.last_name
+            """,
+            data=data,
+        )
 
-def person_template(data):
-    # Query to create person nodes and set its properties
-    query = """
-        UNWIND $data as d
-        MERGE (p:Person {personID: d.phone_number})
-          SET p.firstName = d.first_name, p.lastName = d.last_name, p.city = d.city, p.age = d.age
-          SET p.fullName = d.first_name + ' ' + d.last_name
-    """
-    return query
+    @staticmethod
+    def _create_contracts(tx: Transaction, filename: str) -> None:
+        data = parse_input_json(filename)
+        tx.run(
+            """
+            UNWIND $data as d
+            MATCH (p:Person {personID: d.person_id})
+            MATCH (c:Company {name: d.company_name})
+            MERGE (p) -[r:HAS_CONTRACT]-> (c)
+            """,
+            data=data,
+        )
 
-
-def contract_template(data):
-    # Query to create relationships between company and person
-    query = """
-        UNWIND $data as d
-        MATCH (p:Person {personID: d.person_id})
-        MATCH (c:Company {name: d.company_name})
-        MERGE (p) -[r:HAS_CONTRACT]-> (c)
-    """
-    return query
-
-
-def call_template(data):
-    # Query to create relationships between persons who called each other
-    query = """
-        UNWIND $data AS d
-        MERGE (p1:Person {personID: d.caller_id})
-        MERGE (p2:Person {personID: d.callee_id})
-        WITH p1, p2, d, toUpper(replace(split(d.started_at, 'T')[0], '-', '_')) AS rel_type
-          CALL apoc.merge.relationship(p1, 'CALL_' + rel_type,
-               {started_at: datetime(d.started_at), call_duration: d.duration}, NULL, p2,
-               {started_at: datetime(d.started_at), call_duration: d.duration}
-          )
-          YIELD rel
-        RETURN d
-    """
-    return query
+    @staticmethod
+    def _create_calls(tx: Transaction, filename: str) -> None:
+        data = parse_input_json(filename)
+        tx.run(
+            """
+            UNWIND $data AS d
+            MERGE (p1:Person {personID: d.caller_id})
+            MERGE (p2:Person {personID: d.callee_id})
+            WITH p1, p2, d, toUpper(replace(split(d.started_at, 'T')[0], '-', '_')) AS rel_type
+              CALL apoc.merge.relationship(p1, 'CALL_' + rel_type,
+                  {started_at: datetime(d.started_at), call_duration: d.duration}, NULL, p2,
+                  {started_at: datetime(d.started_at), call_duration: d.duration}
+              )
+              YIELD rel
+            RETURN d
+            """,
+            data=data,
+        )
 
 
 def parse_input_json(filename):
-    # Return JSON data
     with open(filename) as f:
         data = json.load(f)
     return data
 
 
-# The filenames and methods we wish to use in this graph build operation
-INPUTS = [
-    {
-        "file": "data/companies.json",
-        "template": company_template
-    },
-    {
-        "file": "data/people.json",
-        "template": person_template
-    },
-    {
-        "file": "data/contracts.json",
-        "template": contract_template
-    },
-    {
-        "file": "data/calls.json",
-        "template": call_template
-    },
-]
-
 if __name__ == "__main__":
-    build_graph()
+    filenames = {
+        "companies": "data/companies.json",
+        "people": "data/people.json",
+        "contracts": "data/contracts.json",
+        "calls": "data/calls.json",
+    }
+    # Start connection and run build queries
+    connection = Neo4jConnection(
+        uri="bolt://localhost:7687",
+        user="neo4j",
+        password="12345",
+        filenames=filenames,
+    )
+    print("Building graph...")
+    connection.run()
+    connection.close()
+    print("Finished! Successfully imported data into Neo4j!")
--- phone_calls/neo4j_queries.py
+++ phone_calls/neo4j_queries.py
@@ -1,100 +1,95 @@
 """
 Run custom Cypher queries on Neo4j graph to answer questions based on business case.
 """
-from py2neo import Graph
+from time import time
+from neo4j import GraphDatabase, BoltDriver
 
-uri = "bolt://localhost:7687"
+
+def query_1(driver: BoltDriver, **params) -> None:
+    with driver.session() as session:
+        query = """
+            MATCH (callee:Person {personID: $poi}) <-[r]- (caller:Person)
+            WHERE r.started_at > datetime($timestamp)
+            WITH DISTINCT caller AS callers
+            RETURN collect(callers.personID) AS callers
+        """
+        print(f"\nQuery 1:\n {query}")
+        result = session.run(query, params)
+        print(f"Result:\n{result.data()}")
 
 
-def run_queries():
-    # Start a Neo4j graph session and pass Cypher query strings to graph
-    graph = Graph(uri, user="neo4j", password="local")
+def query_2(driver: BoltDriver, **params) -> None:
+    with driver.session() as session:
+        query = """
+            MATCH (c:Company {name: $company}) <-[:HAS_CONTRACT]- (suspect:Person {city: $city}) -[r1]-> (:Person)
+            WHERE suspect.age > $suspect_age
+            WITH suspect, r1.started_at AS patternDate
+            MATCH (target:Person) <-[r2]- (suspect)
+            WHERE target.age < $target_age AND r2.started_at > patternDate
+            WITH DISTINCT target AS t
+            RETURN collect(t.personID) AS targets
+        """
+        print(f"\nQuery 2:\n {query}")
+        result = session.run(query, params)
+        print(f"Result:\n{result.data()}")
+
+
+def query_3(driver: BoltDriver, **params) -> None:
+    with driver.session() as session:
+        query = """
+            MATCH (p1:Person {personID: $person1}) -[:HAS_CONTRACT]-> (c:Company {name: $company})
+            MATCH (p2:Person {personID: $person2}) -[:HAS_CONTRACT]-> (c)
+            MATCH (p1) --> (contact:Person) <-- (p2)
+            WITH DISTINCT contact AS c
+            RETURN collect(c.personID) AS commonContacts
+        """
+        print(f"\nQuery 3:\n {query}")
+        result = session.run(query, params)
+        print(f"Result:\n{result.data()}")
+
+
+def query_4(driver: BoltDriver, **params) -> None:
+    with driver.session() as session:
+        query = """
+            MATCH (poi:Person {personID: $poi})
+            MATCH (c:Company {name: $company}) <-- (p:Person) --> (poi)
+            WITH collect(p) AS callers
+              UNWIND callers AS caller
+              UNWIND callers AS callee
+            WITH *
+              MATCH (caller) -- (callee)
+            WITH DISTINCT caller AS c
+              RETURN collect(c.personID) AS callers
+        """
+        print(f"\nQuery 4:\n {query}")
+        result = session.run(query, params)
+        print(f"Result:\n{result.data()}")
+
+
+def query_5(driver: BoltDriver, **params) -> None:
+    with driver.session() as session:
+        query = """
+            MATCH (company:Company {name: $company}) <-[:HAS_CONTRACT]- (customer:Person) -[called]-> (p:Person)
+            WHERE customer.age > $age AND EXISTS (called.call_duration)
+            RETURN customer.fullName as name, avg(called.call_duration) AS avgCallDuration
+            ORDER BY avgCallDuration DESC LIMIT 3
+        """
+        print(f"\nQuery 5:\n {query}")
+        result = session.run(query, params)
+        print(f"Result:\n{result.data()}")
+
+
+def main() -> None:
+    start_time = time()
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "12345"))
     # Run required queries for inference
-    query_1(graph, poi="+86 921 547 9004", timestamp="2018-09-14T17:18:49")
-    query_2(graph, company='Telecom', city='London', suspect_age=50, target_age=20)
-    query_3(graph, company='Telecom', person1='+7 171 898 0853', person2='+370 351 224 5176')
-    query_4(graph, company='Telecom', poi='+48 894 777 5173')
-    query_5(graph, company='Telecom', age=20, operator='<')
-    query_5(graph, company='Telecom', age=40, operator='>')
-
-
-def query_1(graph, **params):
-    query = """
-        MATCH (callee:Person {personID:'%(poi)s'}) <-[r]- (caller:Person)
-        WHERE r.started_at > datetime('%(timestamp)s')
-        WITH DISTINCT caller AS callers
-        RETURN collect(callers.personID) AS callers
-    """ % {"poi": params['poi'], "timestamp": params['timestamp']}
-    print(f"\nQuery 1:\n {query}")
-    result = graph.run(query, params)
-    result = result.data()[0]   # Consume cursor and extract result
-    print(f"Result:\n{result}")
-
-
-def query_2(graph, **params):
-    query = """
-        MATCH (c:Company {name:'%(company)s'}) <-[:HAS_CONTRACT]- (suspect:Person {city:'%(city)s'}) -[r1]-> (:Person)
-        WHERE suspect.age > %(suspect_age)s
-        WITH suspect, r1.started_at AS patternDate
-        MATCH (target:Person) <-[r2]- (suspect)
-        WHERE target.age < %(target_age)s AND r2.started_at > patternDate
-        WITH DISTINCT target AS t
-        RETURN collect(t.personID) AS targets
-    """ % {
-        "company": params['company'], "city": params['city'],
-        "suspect_age": params['suspect_age'], "target_age": params['target_age']
-    }
-    print(f"\nQuery 2:\n {query}")
-    result = graph.run(query, params)
-    result = result.data()   # Consume cursor and extract result
-    print(f"Result:\n{result}")
-
-
-def query_3(graph, **params):
-    query = """
-        MATCH (p1:Person {personID:'%(person1)s'}) -[:HAS_CONTRACT]-> (c:Company {name:'%(company)s'})
-        MATCH (p2:Person {personID:'%(person2)s'}) -[:HAS_CONTRACT]-> (c)
-        MATCH (p1) --> (contact:Person) <-- (p2)
-        WITH DISTINCT contact AS c
-        RETURN collect(c.personID) AS commonContacts
-    """ % {
-        "company": params['company'], "person1": params['person1'], "person2": params['person2']
-    }
-    print(f"\nQuery 3:\n {query}")
-    result = graph.run(query, params)
-    result = result.data()[0]   # Consume cursor and extract result
-    print(f"Result:\n{result}")
-
-
-def query_4(graph, **params):
-    query = """
-        MATCH (poi:Person {personID:'%(poi)s'})
-        MATCH (c:Company {name:'%(company)s'}) <-- (p:Person) --> (poi)
-        WITH collect(p) AS callers
-        UNWIND callers AS caller
-        UNWIND callers AS callee
-        WITH *
-        MATCH (caller) -- (callee)
-        WITH DISTINCT caller AS c
-        RETURN collect(c.personID) AS callers
-    """ % {"company": params['company'], "poi": params['poi']}
-    print(f"\nQuery 4:\n {query}")
-    result = graph.run(query, params)
-    result = result.data()[0]   # Consume cursor and extract result
-    print(f"Result:\n{result}")
-
-
-def query_5(graph, **params):
-    query = """
-        MATCH (company:Company {name:'%(company)s'}) <-[:HAS_CONTRACT]- (customer:Person) -[called]-> (p:Person)
-        WHERE customer.age %(operator)s %(age)s AND EXISTS (called.call_duration)
-        RETURN avg(called.call_duration) AS avgCallDuration
-    """ % {"company": params['company'], "age": params['age'], 'operator': params['operator']}
-    print(f"\nQuery 5:\n {query}")
-    result = graph.run(query, params)
-    result = result.data()[0]   # Consume cursor and extract result
-    print(f"Result:\n{result}")
+    query_1(driver, poi="+86 921 547 9004", timestamp="2018-09-14T17:18:49")
+    query_2(driver, company='Telecom', city='London', suspect_age=50, target_age=20)
+    query_3(driver, company='Telecom', person1='+7 171 898 0853', person2='+370 351 224 5176')
+    query_4(driver, company='Telecom', poi='+48 894 777 5173')
+    query_5(driver, company='Telecom', age=40)
+    print(f"Ran queries in {time() - start_time:.2f} seconds")
 
 
 if __name__ == "__main__":
-    run_queries()
+    main()
